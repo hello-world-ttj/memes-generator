4acda39ffec570a78ced843c23023ff016c5b915
Tijo T Joseph <tijotjoseph@Tijos-MacBook-Air.local>
2021-11-15 19:40:03 +0530

    React First Static Page Created

---
+++ src/Content.js
@@ -0,0 +1,87 @@
+import './Content.css';
+
+export default function Content() {
+    return (
+        <div className="Container">
+            <p> <strong>Kurup</strong> is a 2021 Indian Malayalam-language biographical-crime thriller film written by K. S. Aravind,
+                Jithin K. Jose and Daniel Sayooj Nair based on the fugitive Sukumara Kurup and directed by Srinath Rajendran;
+                starring Dulquer Salmaan in the title role along with Indrajith Sukumaran, Sobhita Dhulipala, Shine Tom Chacko,
+                Sunny Wayne, Bharath Srinivasan, Surabhi Lakshmi,Vijayaraghavan and Tovino Thomas.
+                The film score and songs are composed by Sushin Shyam.</p>
+            <hr className="Line" />
+            <h2 className="Cast">&#124; Top Cast &#8667;</h2>
+            <div className="TopCast">
+            <div class="Card">
+            <a href="https://www.instagram.com/dqsalmaan/"><img className="InstaIcon" alt="Insta_logo" src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v2.png"/></a>
+                <div class ="Title_div">
+                <h4>Dulquer Salmaan</h4>
+                </div>
+            </div>
+
+            <div class="Card">
+            <a href="https://www.instagram.com/indrajith_s/"><img className="InstaIcon" alt="Insta_logo" src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v2.png"/></a>
+                <div class ="Title_div">
+                <h4>Indrajith Sukumaran</h4>
+                </div>
+            </div>
+
+            <div class="Card">
+            <a href="https://www.instagram.com/sobhitad/"><img className="InstaIcon" alt="Insta_logo" src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v2.png"/></a>
+                <div class ="Title_div">
+                <h4>Sobhita Dhulipala</h4>
+                </div>
+            </div>
+
+            <div class="Card">
+            <a href="https://www.instagram.com/shinetomchacko_official/"><img className="InstaIcon" alt="Insta_logo" src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v2.png"/></a>
+                <div class ="Title_div">
+                <h4>Shine Tom Chacko</h4>
+                </div>
+            </div>
+
+            <div class="Card">
+            <a href="https://www.instagram.com/sunnywayn/"><img className="InstaIcon" alt="Insta_logo" src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v2.png"/></a>
+                <div class ="Title_div">
+                <h4>Sunny Wayne</h4>
+                </div>
+            </div>
+
+            <div class="Card">
+            <a href="https://www.instagram.com/tovinothomas/"><img className="InstaIcon" alt="Insta_logo" src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v2.png"/></a>
+                <div class ="Title_div">
+                <h4>Tovino Thomas</h4>
+                </div>
+            </div>
+
+            <div class="Card">
+            <a href="https://www.instagram.com/bharath_niwas/"><img className="InstaIcon" alt="Insta_logo" src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v2.png"/></a>
+                <div class ="Title_div">
+                <h4>Bharath Srinivasan</h4>
+                </div>
+            </div>
+
+            <div class="Card">
+            <a href="https://www.instagram.com/aanandbal/"><img className="InstaIcon" alt="Insta_logo" src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v2.png"/></a>
+                <div class ="Title_div">
+                <h4>Anand Bal</h4>
+                </div>
+            </div>
+
+            <div class="Card">
+            <a href="https://www.instagram.com/surabhi_lakshmi/"><img className="InstaIcon" alt="Insta_logo" src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v2.png"/></a>
+                <div class ="Title_div">
+                <h4>Surabhi Lakshmi</h4>
+                </div>
+            </div>
+
+            <div class="Card">
+            <a href="https://www.instagram.com/anupamaparameswaran96/"><img className="InstaIcon" alt="Insta_logo" src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v2.png"/></a>
+                <div class ="Title_div">
+                <h4>Anupama Parameswaran</h4>
+                </div>
+            </div>
+
+            </div>
+        </div>
+    )
+}
