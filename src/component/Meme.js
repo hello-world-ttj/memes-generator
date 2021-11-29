f28df23b7e4a1e42f3be5c46097f43d53050a382
Tijo T Joseph <tijotjoseph@Tijos-MacBook-Air.local>
2021-11-29 19:38:43 +0530

    Meme generator completed🥳

--- src/component/Meme.js
+++ src/component/Meme.js
@@ -1,12 +1,38 @@
+import React , {useState,useEffect} from "react"
 import "./Meme.css"
-import MemesData from "./MemesData"
 
 export default function Meme() {
+
+    const [meme, setMeme] = useState({
+        topText : "",
+        bottomText : "",
+        randomImage : "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemes, setAllMemes] = useState([])
+
+
+    useEffect(() => {
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMemes()
+    }, [])
+
+
     function getMemeImage() {
-        const memesArr = MemesData.data.memes
-        const randomNumber = Math.floor(Math.random()*memesArr.length + 1)
-        const memesUrl = memesArr[randomNumber].url
-        console.log(memesUrl);
+        const randomNumber = Math.floor(Math.random()*allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme({...meme, randomImage:url})
+        // console.log(meme.randomImage);
+    }
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        // console.log(value,name);
+        setMeme({...meme, [name]: value})
     }
     
     return (
@@ -16,11 +42,17 @@ export default function Meme() {
                     type="text"
                     placeholder="Top text"
                     className="Form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
                 <input 
                     type="text"
                     placeholder="Bottom text"
                     className="Form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button 
                     className="Form--btn"
@@ -29,6 +61,14 @@ export default function Meme() {
                     Get a new meme image 🖼
                 </button>
             </div>
+            <div className="Meme">
+                <img src={meme.randomImage} className="Meme--image" alt="Meme"/>
+                <h2 className="Meme--text top">{meme.topText}</h2>
+                <h2 className="Meme--text bottom">{meme.bottomText}</h2>
+            </div>
+            <footer class="Footer">
+                <span>© hello_world.ttj</span>
+            </footer>
         </main>
     )
 }
